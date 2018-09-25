08b657611fb77926bf9dcbd542e6241552f93bb5
Dewansh Rawat <dewanshrawat15@gmail.com>
2018-09-26 00:58:33 +0530

    Mailing Script Bug Free

--- mail/mail.py
+++ mail/mail.py
@@ -2,8 +2,11 @@ import smtplib
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("dewanshrawat15@gmail.com", "477FF945de")
- 
-msg = "BHSDK!"
-server.sendmail("dewanshrawat15@gmail.com", "dewanshrawat15@gmail.com", msg)
+server.login("emailaddress", "pwd") # (emailID, password)
+
+arr = ["shashankkarmakar2000@gmail.com", "dewanshrawat15@gmail.com", "dewanshrawat1501@gmail.com", "lucifer15jan@gmail.com"];
+
+msg = "I'm a web designer at Converge Clan, which is a prestigious group of highly talented people. I've been practising my skills for quite some time. I first analyse then proceed on a problem. I've included some of my work below. I'm your best shot for any work that has to be done." #Custom Message
+for x in arr:
+	server.sendmail("emailaddress", x, msg)
 server.quit()
